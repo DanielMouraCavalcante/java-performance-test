ff2d0f57c684050a1d4383b24d3aeafc27395c23
Daniel Cavalcante <daniel.cavalcante@picpay.com>
2020-12-13 17:38:53 -0300

    Testando Pool de Strings

--- src/br/com/dnkt/javaperformance/exploringstrings/Main.java
+++ src/br/com/dnkt/javaperformance/exploringstrings/Main.java
@@ -2,6 +2,7 @@ package br.com.dnkt.javaperformance.exploringstrings;
 
 public class Main {
 
+	//Exemplo de String Pool da JVM
 	public static void main(String[] args) {
 		String one = "hello";
 		String two = "hello";
@@ -10,11 +11,19 @@ public class Main {
 		System.out.println(one == two);
 		
 		Integer i = 76;
-		String three = i.toString().intern();
+		String three = i.toString();
 		String four = "76";
 		
 		System.out.println(three.equals(four));
 		System.out.println(three == four);
+
+
+		Integer i2 = 99;
+		String five = i2.toString().intern(); // guaranteed to be from a pool of unique strings
+		String six = "99";
+
+		System.out.println(five.equals(six));
+		System.out.println(five == six);
 		
 	}
 
